6b42769715474f35f67173c83a64314b37afb9da
Elliot Espinoza <eabraham.espinoza@gmail.com>
2024-08-07 15:33:04 -0400

    Set Up Location Function

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,29 @@ env.config()
 const port = process.env.PORTSERVER;
 app.use(cors());
 
+const getTimelineURL = "https://api.tomorrow.io/v4/timelines"
+const apiKey = "dkclAagD70e5HeCECrzh98dNP3OLoEng"
+
+function getCurrentLocation(){
+    return new Promise((resolve, reject) => {
+        if(!navigator.geolocation){
+            reject(new Error('Geolocation is not supported by your browser'))
+        } else {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const {latitude, longitude} = position.coords;
+                    resolve({latitude, longitude})
+                },
+                (error) => reject(error),
+                {
+                    enableHighAccuracy: true,
+                    timeout: 5000,
+                    maximumAge: 0
+                }
+            )
+        }
+    })
+}
 
 app.get("/test", (req, res) => {
     res.json("Hello World")
