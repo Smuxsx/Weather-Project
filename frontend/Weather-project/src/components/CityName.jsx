c880fa33054be8a26f9d9350d342d8a38fa0ce11
Elliot Espinoza <eabraham.espinoza@gmail.com>
2024-08-08 15:07:52 -0400

    Move the CityName div into a component for WeatherContent component reusability

---
+++ frontend/Weather-project/src/components/CityName.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CityName(){
+    return  <div className="CityName">
+        <h1 style={{fontFamily: "Edu AU VIC WA NT Hand", fontSize: "2rem"}}>London</h1>
+    </div>
+}
+
+export default CityName;
--- frontend/Weather-project/src/components/WeatherContent.jsx
+++ frontend/Weather-project/src/components/WeatherContent.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import CityName from './CityName';
 
 function WeatherContent(props){
     return <div>
-        <div className="CityName">
-            <h1 style={{fontFamily: "Edu AU VIC WA NT Hand", fontSize: "2rem"}}>London</h1>
-        </div>
+        <CityName />
         <div className='CurrentWeather'>
             <WbSunnyIcon className="WeatherIcon" style={{ fontSize: "3.5rem"}}/>
             <pre className='Data'>{props.temperature + "°"}</pre>
