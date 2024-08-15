32fcb40b9309af4b54bfbbe3d32b3abd5e982f2e
Elliot Espinoza <eabraham.espinoza@gmail.com>
2024-08-15 19:55:47 -0400

    Style the input to increase it's text size

--- frontend/Weather-project/src/components/Header.jsx
+++ frontend/Weather-project/src/components/Header.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import StormIcon from '@mui/icons-material/Storm';
 import TextField from '@mui/material/TextField';
-import SendIcon from '@mui/icons-material/Send';
 import Button from '@mui/material/Button';
-
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header(){
     return <header className='Header'>
@@ -12,8 +11,24 @@ function Header(){
                 <h1 className='Icon'><StormIcon fontSize='large'/> Weather App</h1>
             </div>
             <form action="/postData" method="post">
-                <TextField id="filled-basic" label="Enter a city Name" variant="filled" name='CityName'/>
-                <Button variant="contained" style={{height: "55px"}} endIcon={<SendIcon />}>
+                <TextField
+                 id="filled-basic"
+                 label="Enter a city Name"
+                 style={{width: "400px"}}
+                 variant="filled" 
+                 name='CityName'
+                 InputProps={{
+                    style: {
+                        fontSize: '20px',
+                    },
+                 }}
+                 InputLabelProps={{
+                    style: {
+                        fontSize: '20px',
+                    },
+                 }}
+                 />
+                <Button variant="contained" style={{height: "60px"}} endIcon={<SearchIcon />}>
                     Search
                 </Button>
             </form>
